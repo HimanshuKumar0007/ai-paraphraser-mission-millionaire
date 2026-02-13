945ceb1669881bebe5ee557a9275e8d9b4e6c776
HimanshuKumar0007 <140370652+HimanshuKumar0007@users.noreply.github.com>
2026-02-13 19:44:38 +0530

    Enable prepared statements for Supabase Direct Connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -8,8 +8,7 @@ const connectionString = process.env.DATABASE_URL;
 const sql = postgres(connectionString, {
     ssl: {
         rejectUnauthorized: false
-    },
-    prepare: false
+    }
 });
 
 export default sql;
