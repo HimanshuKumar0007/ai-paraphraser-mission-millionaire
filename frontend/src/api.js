99cd72e47426ffc0dd57b0ff55a5ca81279e9300
HimanshuKumar0007 <140370652+HimanshuKumar0007@users.noreply.github.com>
2026-02-14 19:21:24 +0530

    Final frontend URL fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-paraphraser-mission-millionaire-production.up.railway.app",
+  baseURL: "https://api.wordlyai.pro",
 });
 
 API.interceptors.request.use((req) => {
